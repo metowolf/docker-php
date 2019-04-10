6019a5a0d57960f43c71b080033b8c835de84420
metowolf <i@i-meto.com>
2019-04-10 16:36:33 +0800

    update travis build

--- build.sh
+++ build.sh
@@ -2,13 +2,29 @@
 
 set -e
 
-VERSION=`cat Dockerfile | grep 'ARG PHP_VERSION' | awk -F '=' '{print $2}'`
+VERSION=`cat Dockerfile | grep ${VERSION_KEYWORD} | awk -F '=' '{print $2}'`
+MAJOR=`echo ${VERSION} | cut -d. -f1`
+MINOR=`echo ${VERSION} | cut -d. -f2`
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build -t metowolf/php .
-
+docker build --no-cache -t ${IMAGE_NAME}:travis .
 docker images
 
-docker push metowolf/php
-docker tag metowolf/php metowolf/php:$VERSION
-docker push metowolf/php:$VERSION
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+
+  if [ "$TRAVIS_BRANCH" == "master" ]; then
+
+    docker tag ${IMAGE_NAME}:travis ${IMAGE_NAME}:latest
+    docker push ${IMAGE_NAME}:latest
+
+    docker tag ${IMAGE_NAME}:travis ${IMAGE_NAME}:${MAJOR}
+    docker push ${IMAGE_NAME}:${MAJOR}
+
+    docker tag ${IMAGE_NAME}:travis ${IMAGE_NAME}:${MAJOR}.${MINOR}
+    docker push ${IMAGE_NAME}:${MAJOR}.${MINOR}
+
+  fi
+
+  docker tag ${IMAGE_NAME}:travis ${IMAGE_NAME}:$VERSION
+  docker push ${IMAGE_NAME}:$VERSION
+
+fi
